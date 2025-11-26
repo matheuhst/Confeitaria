292f09f678c560ece29093cfa15e401841368f74
matheuhst <mmmatheus.fsouza@gmail.com>
2025-11-26 20:14:23 -0300

    criando item com js

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,34 @@
-alert('oi, fui carregado')
+// Criando elementos HTML no JavaScript dinamicamente
+function criarItemCardapio(titulo, descricao, foto){
+
+    const divItemCardapio = document.createElement('div')
+    divItemCardapio.className = 'item-cardapio'
+
+    const h3Titulo = document.createElement('h3')
+    h3Titulo.textContent = titulo
+
+    const pDescricao = document.createElement('p')
+    pDescricao.textContent = descricao
+    pDescricao.className = 'descricao'
+    // add a classe
+
+    const img = document.createElement('img')
+    img.scr = foto
+    img.className = 'img-item'
+
+    // add elementos na div mãe
+
+    const divC = document.getElementById('cardapio')
+
+    divItemCardapio.appendChild(h3Titulo)
+    divItemCardapio.appendChild(pDescricao)
+    divItemCardapio.appendChild(img)
+
+    divC.appendChild(divItemCardapio)
+}
+
+criarItemCardapio(
+    'Bolo de Chocolate',
+    'Um clássico irresistível com camadas de chocolate',
+    'https://pt.petitchef.com/imgupl/recipe/bolo-de-chocolate-humido-e-fofinho--lg-454177p704082.webp'
+)
