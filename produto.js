523b5be69859b9b294f8a4fff18e22e3baaa1944
matheuhst <mmmatheus.fsouza@gmail.com>
2026-03-03 06:41:17 -0300

    Colocando para pegar todos os produtos

--- produto.js
+++ produto.js
@@ -18,7 +18,22 @@ const produtos = {
         nome: "Bolo de Cenoura",
         descricao: "Delícia tradicional com cobertura de chocolate.",
         imagem: "https://imagem.band.com.br/novahome/055caa6d-7528-44bc-ac32-3add84bdc0b0.jpg"
-    }
+    },
+    maracuja: {
+        nome: "Bolo de Maracujá",
+        descricao: "Azedo do jeito que qualquer fã de maracujá adora. Experimente!",
+        imagem: "https://truffle-assets.tastemadecontent.net/85b033e6-bolo-mousse-de-maracuja_l_thumb.jpg"
+    },
+    limao: {
+        nome: "Bolo de Limão",
+        descricao: "Melhor opção para sentir o que é azedo e doce na medida certa!",
+        imagem: "https://i.ytimg.com/vi/iS6RhbjBdVY/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAR7pEyLMbfJJ1cnbl4DfM9J2toOw"
+    },
+    laranja: {
+        nome: "Bolo de Laranja",
+        descricao: "Um sabor clássico, agora em sabor doce e macio!",
+        imagem: "https://www.sococo.com.br/app/uploads/2022/01/shutterstock_98381816-1.jpg"
+    },
 };
 
 // Se existir o item
